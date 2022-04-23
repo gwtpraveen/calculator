094a0db0e5902fa53de31a1445bd0fc74cb3e39b
Praveen Thamindu <praveenthamindu@gmail.com>
2022-04-24 00:54:12 +0530

    start calculator functionality

--- src/components/keypad.jsx
+++ src/components/keypad.jsx
@@ -2,27 +2,32 @@ import MainBtn from "./mainBtn";
 import "../style/css/other.css";
 
 const KeyPad = (props) => {
+    const handleNumberKeyPress = () => {};
+    const handleFunctionKeyPress = () => {};
+    const handleEqualKeyPress = () => {};
+    const handleResetKeyPress = () => {};
+
     return ( 
         <div className="keypad">
-            <MainBtn value={7}/>
-            <MainBtn value={8}/>
-            <MainBtn value={9}/>
-            <MainBtn value={"DEL"}/>
-            <MainBtn value={4}/>
-            <MainBtn value={5}/>
-            <MainBtn value={6}/>
-            <MainBtn value={"+"}/>
-            <MainBtn value={1}/>
-            <MainBtn value={2}/>
-            <MainBtn value={3}/>
-            <MainBtn value={"-"}/>
-            <MainBtn value={"."}/>
-            <MainBtn value={0}/>
-            <MainBtn value={"/"}/>
-            <MainBtn value={"x"}/>
+            <MainBtn value={7} onKeyPress={handleNumberKeyPress}/>
+            <MainBtn value={8} onKeyPress={handleNumberKeyPress}/>
+            <MainBtn value={9} onKeyPress={handleNumberKeyPress}/>
+            <MainBtn value={"DEL"} onKeyPress={handleFunctionKeyPress}/>
+            <MainBtn value={4} onKeyPress={handleNumberKeyPress}/>
+            <MainBtn value={5} onKeyPress={handleNumberKeyPress}/>
+            <MainBtn value={6} onKeyPress={handleNumberKeyPress}/>
+            <MainBtn value={"+"} onKeyPress={handleFunctionKeyPress}/>
+            <MainBtn value={1} onKeyPress={handleNumberKeyPress}/>
+            <MainBtn value={2} onKeyPress={handleNumberKeyPress}/>
+            <MainBtn value={3} onKeyPress={handleNumberKeyPress}/>
+            <MainBtn value={"-"} onKeyPress={handleFunctionKeyPress}/>
+            <MainBtn value={"."} onKeyPress={handleNumberKeyPress}/>
+            <MainBtn value={0} onKeyPress={handleNumberKeyPress}/>
+            <MainBtn value={"/"} onKeyPress={handleFunctionKeyPress}/>
+            <MainBtn value={"x"} onKeyPress={handleFunctionKeyPress}/>
             <div className="bottomLine">
-                <MainBtn value={"RESET"}/>
-                <MainBtn value={"="}/>
+                <MainBtn value={"RESET"} onKeyPress={handleResetKeyPress}/>
+                <MainBtn value={"="} onKeyPress={handleEqualKeyPress}/>
             </div>
         </div>
      );
