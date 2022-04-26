a703aa7161f174f14fc63cc53cd02d489b7c3be9
Praveen Thamindu <praveenthamindu@gmail.com>
2022-04-26 20:57:49 +0530

    operators method 1 -> not working fully

--- src/App.js
+++ src/App.js
@@ -1,50 +1,71 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Display from './components/display';
 import KeyPad from './components/keypad';
 import Switch from './components/switch';
 
+let output = 0;
 function App() {
-  const [output, setOutput] = useState(0)
   const [display, setDisplay] = useState(0);
+  const [lastOperator, setLastOperator] = useState("")
+  // const [output, setOutput] = useState(0)
+
 
   const handleInput = (key) => {
-    setDisplay(preVal => {
-      let numb = Number(preVal);
-
-      if (["+", "-", "/" , "x", "=", "RESET"].includes(key)) {
-        if (["RESET", "="].includes(key)) {
-          if (key === "=") {
-            // TODO equal key
-          } else {
-            // TODO reset btn 
-          }
-        } else {
-          if (key === "+") {
-            // TODO additoin 
-          } else if (key === "-") {
-            // TODO subtraction 
-          } else if (key === "/") {
-            // TODO divide
-          } else if (key === "x") {
-            // TODO multiplication 
-          }
+    if (["+", "-", "/" , "x", "=", "RESET"].includes(key)) {
+      // todo operators
+      if (key === "+") {
+        output += display;
+        setLastOperator("+");
+        setDisplay(0);
+      } else if (key === "-") {
+        output -= display;
+        setLastOperator("-")
+        setDisplay(0)
+      } else if (key === "x") {
+        if (output === 0) {
+          output = display;
         }
-  
-      } else {
-        // display user input 
-        numb = String(numb)
-          if (key === "DEL") {
-            numb = numb.slice(0, -1)
-          } else {
-            numb += key;
-          }
+        output = display * output;
+        setLastOperator("x");
+        console.log(output)
+        setDisplay(0)
+      } else if (key === "/") {
+        output /= display;
+        setLastOperator("/");
+        setDisplay(0);
+      } else if (key === "=") {
+        if (lastOperator === "+") {
+          output += display;
+          setDisplay(output)
+        } else if (lastOperator === "-") {
+          output -= display;
+          setDisplay(output);
+        } else if (lastOperator === "x") {
+          output *= display;
+          setDisplay(output);
+        } else if (lastOperator === "/") {
+          output /= display;
+          setDisplay(output);
+        }
+      } else if (key === "RESET") {
+        output = 0;
+        setDisplay(0);
+        setLastOperator(null)
       }
-
-      return (Number(numb));
-    });
+    } else {
+      setDisplay(preVal => {
+        let numb = String(preVal);
+        if (key === "DEL") {
+          numb = numb.slice(0, -1)
+        } else {
+          numb += key;
+        }
+        return (Number(numb))
+      });
+    }
   };
-  
+
   return (
     <>
     <div className='header'>
